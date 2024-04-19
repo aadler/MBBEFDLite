d1fd097220857d7982f12f549f0015d155503c50
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-04-19 12:27:20 +0300

    Cleanup dmb

--- src/dmb.c
+++ src/dmb.c
@@ -3,7 +3,6 @@
 
 
 #include <Rmath.h>
-// #include <Rconfig.h>
 
 #include "dmb.h"
 
@@ -14,31 +13,31 @@ extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP lg) {
   double *px = REAL(x);
   double *pg = REAL(g);
   double *pb = REAL(b);
-  Rboolean pl = asLogical(lg);
 
   SEXP ret = PROTECT(allocVector(REALSXP, n));
   double *pret = REAL(ret);
   memset(pret, 0, n * sizeof(double));
 
   for (R_xlen_t i = 0; i < n; ++i) {
-    R_xlen_t igg = i % gg;
-    R_xlen_t ibb = i % bb;
-    if (pg[igg] < 1.0 | pb[ibb] < 0.0 | ISNAN(px[i])) {
+    R_xlen_t ig = i % gg;
+    R_xlen_t ib = i % bb;
+    double gm1 = pg[ig] - 1.0;
+    if (pg[ig] < 1.0 | pb[ib] < 0.0 | ISNAN(px[i])) {
       pret[i] = R_NaN;
-    } else if (pg[igg] == 1.0 | pb[ibb] == 0.0 | px[i] < 0.0 | px[i] >= 1.0) {
+    } else if (pg[ig] == 1.0 | pb[ib] == 0.0 | px[i] < 0.0 | px[i] >= 1.0) {
       pret[i] = 0.0;
-    } else if (pb[ibb] == 1.0) {
-      pret[i] = (pg[igg] - 1) / pow1p((pg[igg] - 1) * px[i], 2.0);
-    } else if (pb[ibb] * pg[igg] == 1.0) {
-      pret[i] = -log(pb[ibb]) * R_pow(pb[ibb], px[i]);
+    } else if (pb[ib] == 1.0) {
+      pret[i] = gm1 / R_pow_di(gm1 * px[i] + 1.0, 2);
+    } else if (pb[ib] * pg[ig] == 1.0) {
+      pret[i] = -log(pb[ib]) * R_pow(pb[ib], px[i]);
     } else {
-      double b1x = R_pow(pb[ibb], 1 - px[i]);
-      pret[i] = (pb[ibb] - 1) * (pg[igg] - 1) * b1x * log(pb[ibb]) /
-        R_pow_di((pg[igg] - 1) * b1x + 1 - pb[ibb] * pg[igg], 2);
+      double gm1b1x = gm1 * R_pow(pb[ib], 1 - px[i]);
+      pret[i] = (pb[ib] - 1) * gm1b1x * log(pb[ib]) /
+        R_pow_di(gm1b1x + (1 - pb[ib] * pg[ig]), 2);
     }
   }
 
-  if (pl == 1) {
+  if (asLogical(lg) == 1) {
     for (R_xlen_t i = 0; i < n; ++i) {
       pret[i] = log(pret[i]);
     }
