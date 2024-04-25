ec9bb57486228409ac78f549453e2417b4cdc2f9
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-04-25 22:01:08 +0300

    A bit more efficient based on review of internal R code.

--- src/dmb.c
+++ src/dmb.c
@@ -6,13 +6,14 @@
 
 #include "dmb.h"
 
-extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP lg) {
+extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP give_log) {
   const R_xlen_t n = xlength(x);
   const R_xlen_t gg = xlength(g);
   const R_xlen_t bb = xlength(b);
   double *px = REAL(x);
   double *pg = REAL(g);
   double *pb = REAL(b);
+  Rboolean gl = asLogical(give_log);
 
   SEXP ret = PROTECT(allocVector(REALSXP, n));
   double *pret = REAL(ret);
@@ -22,9 +23,11 @@ extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP lg) {
     R_xlen_t ig = i % gg;
     R_xlen_t ib = i % bb;
     double gm1 = pg[ig] - 1.0;
-    if (pg[ig] < 1.0 | pb[ib] < 0.0 | ISNAN(px[i])) {
+    if (ISNA(px[i]) || ISNA(pg[ig]) || ISNA(pb[ib])) {
+      pret[i] = NA_REAL;
+    } else if (pg[ig] < 1.0 || pb[ib] < 0.0 || ISNAN(px[i] + pg[ig] + pb[ib])) {
       pret[i] = R_NaN;
-    } else if (pg[ig] == 1.0 | pb[ib] == 0.0 | px[i] < 0.0 | px[i] >= 1.0) {
+    } else if (pg[ig] == 1.0 || pb[ib] == 0.0 || px[i] < 0.0 || px[i] >= 1.0) {
       pret[i] = 0.0;
     } else if (pb[ib] == 1.0) {
       pret[i] = gm1 / R_pow_di(gm1 * px[i] + 1.0, 2);
@@ -35,12 +38,8 @@ extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP lg) {
       pret[i] = (pb[ib] - 1) * gm1b1x * log(pb[ib]) /
         R_pow_di(gm1b1x + (1 - pb[ib] * pg[ig]), 2);
     }
-  }
 
-  if (asLogical(lg) == 1) {
-    for (R_xlen_t i = 0; i < n; ++i) {
-      pret[i] = log(pret[i]);
-    }
+    pret[i] = gl ? log(pret[i]) : pret[i];
   }
 
     UNPROTECT(1);
--- src/pmb.c
+++ src/pmb.c
@@ -6,13 +6,15 @@
 
 #include "pmb.h"
 
-extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lt, SEXP lg) {
+extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lower_tail, SEXP log_p) {
   const R_xlen_t n = xlength(q);
   const R_xlen_t gg = xlength(g);
   const R_xlen_t bb = xlength(b);
   double *pq = REAL(q);
   double *pg = REAL(g);
   double *pb = REAL(b);
+  Rboolean lt = asLogical(lower_tail);
+  Rboolean lp = asLogical(log_p);
 
   SEXP ret = PROTECT(allocVector(REALSXP, n));
   double *pret = REAL(ret);
@@ -23,9 +25,9 @@ extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lt, SEXP lg) {
     R_xlen_t ib = i % bb;
     double gm1 = pg[ig] - 1.0;
     double gb = pg[ig] * pb[ib];
-    if (ISNA(pq[i])) {
+    if (ISNA(pq[i]) || ISNA(pg[ig]) || ISNA(pb[ib])) {
       pret[i] = NA_REAL;
-    } else if (pg[ig] < 1.0 | pb[ib] < 0.0 | ISNAN(pq[i])) {
+    } else if (pg[ig] < 1.0 || pb[ib] < 0.0 || ISNAN(pq[i] + pg[ig] + pb[ib])) {
       pret[i] = R_NaN;
     } else if (pq[i] >= 1.0) {
       pret[i] = 1.0;
@@ -39,18 +41,9 @@ extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lt, SEXP lg) {
       double gm1b1x = gm1 * R_pow(pb[ib], 1 - pq[i]);
       pret[i] = 1.0 - (1.0 - pb[ib]) / (gm1b1x + 1.0 - gb);
     }
-  }
 
-  if (asLogical(lt) == 0) {
-    for (R_xlen_t i = 0; i < n; ++i) {
-      pret[i] = 1.0 - (pret[i]);
-    }
-  }
-
-  if (asLogical(lg) == 1) {
-    for (R_xlen_t i = 0; i < n; ++i) {
-      pret[i] = log(pret[i]);
-    }
+    pret[i] = lt ? (pret[i]) : 0.5 - (pret[i]) + 0.5; // See dpq.h
+    pret[i] = lp ? log(pret[i]) : pret[i];
   }
 
     UNPROTECT(1);
