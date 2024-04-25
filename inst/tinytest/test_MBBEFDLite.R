d47685f1f99f056db1d06f939e41480d37b566de
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-04-25 22:01:43 +0300

    Simplify test suite.

--- inst/tinytest/test_MBBEFDLite.R
+++ inst/tinytest/test_MBBEFDLite.R
@@ -2,24 +2,23 @@
 # SPDX-License-Identifier: MPL-2.0+
 
 tol <- 10 * .Machine$double.eps
+x <- c(NaN, NA_real_, seq(-0.05, 1.05, .05))
 
 ############################### Testing dmb ####################################
-x <- c(0.069360915804281831, 0.81777519872412086, 0.94262173236347735,
-       0.26938187633641064, 0.16934812325052917)
-y <- c(-1.2, -1.0, 0.0,  0.5,  0.9,  1.0,  1.2, NaN, NA)
-
 # Standard b & g and passing log
 g <- 20
 b <- 0.5
-controlx <- c(3.6876104357149244, 0.096737198074403216, 0.073812924647130657,
-             0.6609736077485624, 1.3068098071845686)
-controly <- c(0, 0, 13.169796430638961, 0.23672338994222369,
-              0.080659166235059657, 0, 0, NaN, NA)
+dcontrolx <- c(NaN, NA, 0, 13.169796430638961, 4.8885512023122937,
+               2.5263813046917969, 1.5385762756474961, 1.0338586295606644,
+               0.74178454647271053, 0.55773712910156925, 0.43433663080658902,
+               0.34759405724528358, 0.28430720436213047, 0.23672338994222369,
+               0.20004716240988563, 0.1711831300348825, 0.14806084939852232,
+               0.12925298240848238, 0.1137496715734246, 0.10082014212059009,
+               0.089925016722583739, 0.080659166235059657, 0.072713521352461077,
+               0, 0)
 
-expect_equal(dmb(x, g, b), controlx, tolerance = tol)
-expect_equal(dmb(x, g, b, TRUE), log(controlx), tolerance = tol)
-expect_equal(dmb(y, g, b), controly, tolerance = tol)
-expect_equal(dmb(y, g, b, TRUE), log(controly), tolerance = tol)
+expect_equal(dmb(x, g, b), dcontrolx, tolerance = tol)
+expect_equal(dmb(x, g, b, TRUE), log(dcontrolx), tolerance = tol)
 
 # Nonstandard g & b
 ## g < 1 and b < 0
@@ -39,32 +38,33 @@ expect_equal(dmb(0.5, 5, 0.2), 0.71976251555360038, tolerance = tol)
 # Test vectorized b & g
 g <- c(1.2, 4, 100)
 b <- c(0.001, 0.17)
-controlx <- c(dmb(x[1L], g[1L], b[1L]),
+dcontrolx <- c(dmb(x[1L], g[1L], b[1L]),
               dmb(x[2L], g[2L], b[2L]),
               dmb(x[3L], g[3L], b[1L]),
               dmb(x[4L], g[1L], b[2L]),
-              dmb(x[5L], g[2L], b[1L]))
+              dmb(x[5L], g[2L], b[1L]),
+              dmb(x[6L], g[3L], b[2L]))
 
-expect_identical(dmb(x, g, b), controlx)
+expect_identical(dmb(x, g, b)[1:6], dcontrolx)
 
 ############################### Testing pmb ####################################
 # Standard b & g
 g <- 20
 b <- 0.5
-controlx <- c(0.48341340954506551, 0.93544650077891778, 0.94599947678770424,
-              0.79594064206174842, 0.7029515920261491)
-controly <- c(0, 0, 0, 0.88726116159525426, 0.94271065786700514, 1, 1, NaN, NA)
 
-expect_identical(pmb(x, g, b), controlx)
-expect_identical(pmb(x, g, b, lower.tail = FALSE), 1 - controlx)
-expect_identical(pmb(x, g, b, log.p = TRUE), log(controlx))
-expect_identical(pmb(x, g, b, lower.tail = FALSE, log.p = TRUE),
-                 log(1 - controlx))
-expect_identical(pmb(y, g, b), controly)
-expect_identical(pmb(y, g, b, lower.tail = FALSE), 1 - controly)
-expect_identical(pmb(y, g, b, log.p = TRUE), log(controly))
-expect_identical(pmb(y, g, b, lower.tail = FALSE, log.p = TRUE),
-                 log(1 - controly))
+pcontrolx <- c(NaN, NA, 0, 0, 0.40120963545198918, 0.57693371329906551,
+               0.67551641239099069, 0.73858045887054991, 0.78236907383302978,
+               0.81453124867180904,  0.83914170955380674, 0.85857029312602218,
+               0.8742891819815175, 0.88726116159525426, 0.89814240493843012,
+               0.90739552971536974, 0.91535606623643084, 0.92227329508477796,
+               0.92833628856856798,  0.93369105420497067, 0.93845213614173995,
+               0.94271065786700514,  0.94654001782806174, 1, 1)
+
+expect_equal(pmb(x, g, b), pcontrolx, tolerance = tol)
+expect_equal(pmb(x, g, b, lower.tail = FALSE), 1 - pcontrolx, tolerance = tol)
+expect_equal(pmb(x, g, b, log.p = TRUE), log(pcontrolx), tolerance = tol)
+expect_equal(pmb(x, g, b, lower.tail = FALSE, log.p = TRUE), log(1 - pcontrolx),
+             tolerance = tol)
 
 # Nonstandard g & b
 ## g < 1 and b < 0
