2255c53150acbc4e4d6cfa2babe3656c57c632af
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-05-01 15:09:23 +0300

    Allow the C code to be used by other packages.

--- src/init.c
+++ src/init.c
@@ -19,4 +19,9 @@ void R_init_MBBEFDLite(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
   R_forceSymbols(dll, TRUE);
+  R_RegisterCCallable("MBBEFDLite", "dmb_c",   (DL_FUNC) &dmb_c);
+  R_RegisterCCallable("MBBEFDLite", "pmb_c",   (DL_FUNC) &pmb_c);
+  R_RegisterCCallable("MBBEFDLite", "qmb_c",   (DL_FUNC) &qmb_c);
+  R_RegisterCCallable("MBBEFDLite", "rmb_c",   (DL_FUNC) &rmb_c);
+  R_RegisterCCallable("MBBEFDLite", "ecmb_c",  (DL_FUNC) &ecmb_c);
 }
