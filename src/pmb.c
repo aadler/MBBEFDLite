c6f5ceac46edf7ba3f4d334e28c368cd9eaa280c
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-05-01 13:18:01 +0300

    Cleaner code.

--- src/pmb.c
+++ src/pmb.c
@@ -19,28 +19,27 @@ extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lower_tail, SEXP log_p) {
   memset(pret, 0, n * sizeof(double));
 
   for (R_xlen_t i = 0; i < n; ++i) {
-    R_xlen_t ig = i % gg;
-    R_xlen_t ib = i % bb;
-    double gm1 = pg[ig] - 1.0;
-    double gb = pg[ig] * pb[ib];
-    if (ISNA(pq[i]) || ISNA(pg[ig]) || ISNA(pb[ib])) {
+    double gi = pg[i % gg];
+    double bi = pb[i % bb];
+    double gm1 = gi - 1.0;
+    double gb = gi * bi;
+    if (ISNA(pq[i]) || ISNA(gi) || ISNA(bi)) {
       pret[i] = NA_REAL;
-    } else if (pg[ig] < 1.0 || pb[ib] < 0.0 || ISNAN(pq[i] + pg[ig] + pb[ib])) {
+    } else if (gi < 1.0 || bi < 0.0 || ISNAN(pq[i] + gi + bi)) {
       pret[i] = R_NaN;
     } else if (pq[i] >= 1.0) {
       pret[i] = 1.0;
-    } else if (pg[ig] == 1.0 | pb[ib] == 0.0 | pq[i] < 0.0) {
+    } else if (gi == 1.0 | bi == 0.0 | pq[i] < 0.0) {
       pret[i] = 0.0;
-    } else if (pb[ib] == 1.0) {
+    } else if (bi == 1.0) {
       pret[i] = 1.0 - 1.0 / (1.0 + gm1 * pq[i]);
     } else if (gb == 1.0) {
-      pret[i] = 1.0 - R_pow(pb[ib], pq[i]);
+      pret[i] = 1.0 - R_pow(bi, pq[i]);
     } else {
-      double gm1b1x = gm1 * R_pow(pb[ib], 1 - pq[i]);
-      pret[i] = 1.0 - (1.0 - pb[ib]) / (gm1b1x + 1.0 - gb);
+      pret[i] = 1.0 - (1.0 - bi) / (gm1 * R_pow(bi, 1 - pq[i]) + 1.0 - gb);
     }
 
-    pret[i] = lt ? (pret[i]) : 0.5 - (pret[i]) + 0.5; // See dpq.h
+    pret[i] = lt ? pret[i] : 0.5 - pret[i] + 0.5; // See dpq.h
     pret[i] = lp ? log(pret[i]) : pret[i];
   }
 
