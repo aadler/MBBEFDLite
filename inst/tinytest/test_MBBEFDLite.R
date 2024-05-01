a89c0d5022af4b71767a0af91aad48697d2c0428
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-05-01 13:10:30 +0300

    Finish quantile tests

--- inst/tinytest/test_MBBEFDLite.R
+++ inst/tinytest/test_MBBEFDLite.R
@@ -113,7 +113,31 @@ expect_equal(qmb(p, g, b, lower.tail = FALSE), qmb(omp, g, b), tolerance = tol)
 expect_equal(qmb(logp, g, b, lower.tail = FALSE, log.p = TRUE), qmb(omp, g, b),
              tolerance = tol)
 
-# Edge case b & g and vector
+# Nonstandard g & b
+## g < 1 and b < 0
+expect_true(is.nan(qmb(0.5, 0.2, 6)))
+expect_true(is.nan(qmb(0.5, 1.2, -.3)))
+
+## g == 1 and b == 0
+expect_identical(qmb(0.5, 1, 1), 0)
+expect_identical(qmb(0.5, 1.3, 0), 0)
+
+## b == 1
+expect_equal(qmb(0.25, 4, 1), 0.1111111111111111, tolerance = tol)
+
+## bg == 1
+expect_equal(qmb(0.25, 4, 0.25), 0.20751874963942191, tolerance = tol)
+# Test vectorized b & g
+g <- c(1.2, 4, 100)
+b <- c(0.001, 0.17)
+qcontrolx <- c(qmb(p[1L], g[1L], b[1L]),
+               qmb(p[2L], g[2L], b[2L]),
+               qmb(p[3L], g[3L], b[1L]),
+               qmb(p[4L], g[1L], b[2L]),
+               qmb(p[5L], g[2L], b[1L]),
+               qmb(p[6L], g[3L], b[2L]))
+
+expect_identical(qmb(p, g, b)[1:6], qcontrolx)
 
 ############################### Testing rmb ####################################
 set.seed(9712L)
