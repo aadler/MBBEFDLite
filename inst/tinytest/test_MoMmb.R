a0d9ebde3e9aa0366abfb3bd639a72056005e0b2
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-05-01 20:44:56 +0300

    Add unit tests for MoMmb

---
+++ inst/tinytest/test_MoMmb.R
@@ -0,0 +1,20 @@
+# Copyright Avraham Adler (c) 2024
+# SPDX-License-Identifier: MPL-2.0+
+
+tol <- 10 * .Machine$double.eps
+
+# Test Functionality
+set.seed(138L)
+x <- rmb(1e5, 150, 250)
+testx <- MoMmb(x)
+expect_true(abs(testx$g / 150 - 1) <= 0.02)
+expect_true(abs(testx$b / 250 - 1) <= 0.05)
+
+# Testing simple findb branch
+expect_identical(MoMmb(c(1, 1, 1))$b, 0)
+
+# Test Error Trapping
+set.seed(76L)
+expect_error(MoMmb(rmb(10, 10, 9)), "insufficient data")
+expect_error(MoMmb(rmb(10, 10, 9), tol = 1e-16), "looser tolerance")
+expect_error(MoMmb(NA_real_, na.rm = FALSE), "passed as FALSE")
