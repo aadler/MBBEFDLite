2f553d297f421e8ecdc57ee2d7a6646effda57fb
Avraham Adler <avraham.adler@gmail.com>
2024-05-13 16:01:44 -0400

    More robust implementation and trap clearly obvious failures.

--- R/mom.R
+++ R/mom.R
@@ -19,16 +19,17 @@ mommb <- function(x, maxit = 100L, tol = .Machine$double.eps ^ 0.5,
       return(Inf)
     } else {
       errf <- function(b) {
-        abs(log(g * b) * (1 - b) / (log(b) * (1 - g * b)) - mu)
+        (log(g * b) * (1 - b) / (log(b) * (1 - g * b)) - mu) ^ 2
       }
-      bFit <- optimize(errf, c(0, 1e10))
+      bFit <- optimize(errf, c(0, 1e100))
     }
 
     bFit$minimum
   }
 
   mu <- mean(x, na.rm = na.rm)
-  mu2 <- mean(x ^ 2, na.rm = na.rm)
+  sig <- var(x, na.rm = na.rm)
+  mu2 <- mu ^ 2 + sig
   g <- 1 / mu2
   b <- findb(mu, g, tol)
   converged <- FALSE
@@ -36,23 +37,26 @@ mommb <- function(x, maxit = 100L, tol = .Machine$double.eps ^ 0.5,
   while (!converged && i < maxit) {
     i <- i + 1L
     oldg <- g
-    mu2part <- tryCatch(integrate(\(x) x ^ 2 * dmb(x, g, b), lower = 0,
-                                  upper = 1, subdivisions = 1000L,
-                                  rel.tol = tol)$value,
-                        error = \(cond) simpleError(trimws(cond$message)))
-    if (inherits(mu2part, "simpleError")) {
-      stop(trimws(mu2part$message), "; please try a looser tolerance")
+    intxsqrd <- tryCatch(integrate(\(x) x ^ 2 * dmb(x, g, b), lower = 0,
+                                   upper = 1, subdivisions = 1000L,
+                                   rel.tol = tol)$value,
+                         error = \(cond) simpleError(trimws(cond$message)))
+    if (inherits(intxsqrd, "simpleError")) {
+      stop(trimws(intxsqrd$message), "; perhaps try a looser tolerance.")
     }
 
-    ppart <- mu2 - mu2part
-    if (ppart <= 0) {
-      stop("Algorithm has insufficient data to converge to a method of ",
-           "moments solution.")
+    newp <- mu2 - intxsqrd
+    if (newp <= 0) {
+      newp <- 1 / (g * 2)
     }
-
-    g <- 1 / ppart
+    g <- 1 / newp
     converged <- abs(oldg - g) <= tol
     b <- findb(mu, g, tol)
+  }
+
+  if ((i >= maxit && !converged) || b == 1e100) {
+    stop("Algorithm has insufficient data to converge to a method of ",
+         "moments solution.")
   }
 
   list(g = g, b = b, iter = i,
