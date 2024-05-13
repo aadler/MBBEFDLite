64cc7c9aef70651f985e862b0b24e4f228b1da12
Avraham Adler <avraham.adler@gmail.com>
2024-05-13 16:02:11 -0400

    These contrived examples no longer work.

--- inst/tinytest/test_MoMmb.R
+++ inst/tinytest/test_MoMmb.R
@@ -11,17 +11,10 @@ expect_true(abs(testx$g / 150 - 1) <= 0.02)
 expect_true(abs(testx$b / 250 - 1) <= 0.05)
 
 # Testing simple findb branch
-expect_identical(mommb(c(1, 1, 1))$b, 0)                     # findb returns 0
-# Below is invalid input but findb returns Inf
-expect_error(mommb(c(0.6798293601721539, 1.1838579062372445), tol = 1e-5))
-
-# Below is invalid input but findb returns 1 / g
-expect_error(mommb(c(1.0032341003417968, 1.004322052001954), tol = 1e-5))
-expect_error(mommb(c(0, 0.31824773751438351), tol = 1e-5))   # findb returns 1
+expect_identical(mommb(c(1, 1, 1))$b, 0)                 # findb returns 0
 
 # Test Error Trapping
 set.seed(76L)
 expect_error(mommb(rmb(10, 10, 9)), "insufficient data")
 expect_error(mommb(rmb(10, 10, 9), tol = 1e-16), "looser tolerance")
 expect_error(mommb(NA_real_, na.rm = FALSE), "passed as FALSE")
-
