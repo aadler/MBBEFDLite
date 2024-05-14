c798b8474cd8027f28ecb12a77e9984d990c5036
Avraham Adler <avraham.adler@gmail.com>
2024-05-13 23:35:09 -0400

    Create (malformed) cases to test branches in findb

--- inst/tinytest/test_MoMmb.R
+++ inst/tinytest/test_MoMmb.R
@@ -11,7 +11,12 @@ expect_true(abs(testx$g / 150 - 1) <= 0.02)
 expect_true(abs(testx$b / 250 - 1) <= 0.05)
 
 # Testing simple findb branch
-expect_identical(mommb(c(1, 1, 1))$b, 0)                 # findb returns 0
+expect_identical(mommb(c(1, 1, 1))$b, 0)                    # findb => 0
+x <- c(0.9, 0.9, 0.9, 0.93785026012074624)
+expect_silent(mommb(x, tol = 0.002))                        # findb => 1 / g
+# Malformed input created solely to test branches in findb
+expect_error(mommb(c(0.2, 0.6, (sqrt(2.6) + 1) / 2)))       # findb => Inf
+expect_error(mommb(c(0.2, 0.6, 0.1770747858956514)))        # findb => 1
 
 # Test Error Trapping
 set.seed(76L)
