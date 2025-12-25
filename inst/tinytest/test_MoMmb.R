58f9ac661e08741717e91e686c3ebf701f692dec
Avraham Adler <Avraham.Adler@gmail.com>
2025-12-24 23:03:04 -0500

    Added unit tests to mommb to bring coverage to 100%.

--- inst/tinytest/test_MoMmb.R
+++ inst/tinytest/test_MoMmb.R
@@ -3,12 +3,18 @@
 
 tol <- 10 * .Machine$double.eps
 
-# Test Functionality
+# Test Functionality: m = FALSE
 set.seed(138L)
 x <- rmb(1e5, 150, 250)
 testx <- mommb(x)
 expect_true(abs(testx$g / 150 - 1) <= 0.02)
 expect_true(abs(testx$b / 250 - 1) <= 0.05)
+
+# Test Functionality: m = TRUE
+z <- c(mean(x), mean(x ^ 2))
+testz <- mommb(z, m = TRUE)
+expect_equal(testz$g, testx$g, tolerance = tol)
+expect_equal(testz$b, testx$b, tolerance = tol)
 
 # Testing simple findb branch
 expect_identical(mommb(c(1, 1, 1))$b, 0)                    # findb => 0
@@ -23,3 +29,7 @@ set.seed(76L)
 expect_error(mommb(rmb(10, 10, 9)), "insufficient data")
 expect_error(mommb(rmb(10, 10, 9), tol = 1e-16), "looser tolerance")
 expect_error(mommb(NA_real_, na.rm = FALSE), "passed as FALSE")
+expect_error(mommb(rmb(10, 10, 9), m = TRUE), "first and second raw moments")
+
+# Test trace
+expect_message(mommb(x, trace = TRUE), "i: 1")
