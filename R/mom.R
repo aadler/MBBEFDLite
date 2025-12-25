ee6c4a717766ef46873e0c06d978bdc24c63ee81
Avraham Adler <Avraham.Adler@gmail.com>
2025-12-24 23:01:36 -0500

    1) Brought findb outside of mommb for testing  purposes and expert use. findb is not naturally exported, so added to internal.
    2) Added ability to pass couplet of moments.
    
    3) Added trace
    
    4) Stopped immediately if integral of x^2 f(x) > empirical E[X^2].

--- R/mom.R
+++ R/mom.R
@@ -1,13 +1,32 @@
 # Copyright Avraham Adler (c) 2024
 # SPDX-License-Identifier: MPL-2.0+
 
+findbIntMax <- 1e50
+
+findb <- function(mu, g, tol = .Machine$double.eps ^ 0.5) {
+  if (abs(mu - 1) <= tol) {
+    return(0)
+  } else if (abs(mu - (g - 1) / (log(g) * g)) <= tol) {
+    return(1 / g)
+  } else if (abs(mu - log(g) / (g - 1)) <= tol) {
+    return(1)
+  } else if (abs(mu - 1 / g) <= tol) {
+    return(Inf)
+  } else {
+    errf <- function(b) {
+      (log(g * b) * (1 - b) / (log(b) * (1 - g * b)) - mu) ^ 2
+    }
+    return(optimize(errf, c(.Machine$double.eps, findbIntMax))$minimum)
+  }
+}
+
 mommb <- function(x, m = FALSE, maxit = 100L, tol = .Machine$double.eps ^ 0.5,
-                  na.rm = TRUE) {
+                  na.rm = TRUE, trace = FALSE) {
 
   if (m) {
     if (length(x) != 2L) {
-      stop("Was expecting first and second moment but something other than 2 ",
-           "parameters were passed.")
+      stop("Was expecting first and second raw moments but something other ",
+      "than 2 parameters were passed.")
     }
 
     mu <- x[1L]
@@ -21,51 +40,37 @@ mommb <- function(x, m = FALSE, maxit = 100L, tol = .Machine$double.eps ^ 0.5,
     mu2 <- mean(x ^ 2, na.rm = na.rm)
   }
 
-  findb <- function(mu, g, tol) {
-    if (abs(mu - 1) <= tol) {
-      return(0)
-    } else if (abs(mu - (g - 1) / (log(g) * g)) <= tol) {
-      return(1 / g)
-    } else if (abs(mu - log(g) / (g - 1)) <= tol) {
-      return(1)
-    } else if (abs(mu - 1 / g) <= tol) {
-      return(Inf)
-    } else {
-      errf <- function(b) {
-        (log(g * b) * (1 - b) / (log(b) * (1 - g * b)) - mu) ^ 2
-      }
-      return(optimize(errf, c(0, 1e100))$minimum)
-    }
-  }
-
   g <- 1 / mu2
   b <- findb(mu, g, tol)
   converged <- FALSE
   i <- 0L
   while (!converged && i < maxit) {
+    if (trace) message("i: ", i, "\tg: ", g, "\tb: ", b)
     i <- i + 1L
     oldg <- g
     intxsqrd <- tryCatch(integrate(function(x) {x ^ 2 * dmb(x, g, b)},
                                    lower = 0, upper = 1, subdivisions = 1000L,
                                    rel.tol = tol)$value,
                          error = function(cond) {
-                           simpleError(trimws(cond$message))}
-                         )
+                           simpleError(trimws(cond$message))
+                         })
     if (inherits(intxsqrd, "simpleError")) {
       stop(trimws(intxsqrd$message), "; perhaps try a looser tolerance.")
     }
 
     newp <- mu2 - intxsqrd
-    if (newp > 0) {
-      g <- 1 / newp
-    } else {
-      g <- g * 3 # Force restart keeping 1 / mu2 as upper limit of p
+
+    if (newp <= 0) {
+      stop("Algorithm has insufficient data to converge to a method of ",
+           "moments solution.")
     }
+
+    g <- 1 / newp
     converged <- abs(oldg - g) <= tol
     b <- findb(mu, g, tol)
   }
 
-  if ((i >= maxit && !converged) || b == 1e100) {
+  if ((i >= maxit && !converged) || b == findbIntMax) {
     stop("Algorithm has insufficient data to converge to a method of ",
          "moments solution.")
   }
