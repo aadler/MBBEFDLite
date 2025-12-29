80ae35bc62488c47d1ee82ea5a63cf33bfb697ef
Avraham Adler <Avraham.Adler@gmail.com>
2025-12-29 02:11:15 -0500

    Code consistency

--- src/MBBEFDLite.h
+++ src/MBBEFDLite.h
@@ -11,6 +11,6 @@ extern SEXP dmb_c(SEXP x, SEXP g, SEXP b, SEXP lg);
 extern SEXP pmb_c(SEXP q, SEXP g, SEXP b, SEXP lt, SEXP lg);
 extern SEXP qmb_c(SEXP q, SEXP g, SEXP b, SEXP lt, SEXP lg);
 extern SEXP rmb_c(SEXP n_, SEXP g, SEXP b);
-extern SEXP ecmb_c(SEXP x, SEXP g, SEXP b, SEXP lower_tail);
+extern SEXP ecmb_c(SEXP x, SEXP g, SEXP b, SEXP lt);
 
 #endif
