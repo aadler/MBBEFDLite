ff2761725026ef0efa8f6d13f4d6917a094ed1ca
Avraham Adler <Avraham.Adler@gmail.com>
2026-03-10 00:40:31 -0400

    Cleaner tests

--- inst/tinytest/test_MoMmb.R
+++ inst/tinytest/test_MoMmb.R
@@ -46,7 +46,6 @@ expect_warning(mommb(c(0, 0.5, 1)), "Parameter b is Inf")
 
 # Malformed input created solely to test branches in findb. Values found using
 # ChatGPT
-
 a <- 2 * log(2)
 d <- sqrt(2 - a ^ 2)
 expect_error(mommb(c(a - d, a, a + d)))    # mean must be in [0, 1]
@@ -78,9 +77,9 @@ x2 <- rmb(10, 10, 9)
 expect_error(mommb(x2), "insufficient data")
 expect_error(suppressMessages(mommb(x2, opts = list(alg = "LS"))),
              "insufficient data")
-expect_error(mommb(rmb(10, 10, 9), tol = 1e-16), "looser tolerance")
+expect_error(mommb(x2, tol = 1e-16), "looser tolerance")
 expect_error(mommb(NA_real_, na.rm = FALSE), "passed as FALSE")
-expect_error(mommb(rmb(10, 10, 9), m = TRUE), "other than 2")
+expect_error(mommb(x2, m = TRUE), "other than 2")
 expect_error(mommb(x, opts = list(alg = "sqrt")), "Algorithm must be")
 
 # Test trace
